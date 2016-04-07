3fc1cfd67ec71e34800be5d13ac32510c5b7274a
jun <1042439471@qq.com>
2016-04-07 23:41:05 +0800

    Site updated: 2016-04-07 23:41:05

--- js/script.js
+++ js/script.js
@@ -134,4 +134,22 @@
 
     $container.removeClass('mobile-nav-on');
   });
+  skel.breakpoints({
+    xlarge: "(max-width: 1680px)",
+    large:  "(max-width: 1280px)",
+    medium: "(max-width: 980px)",
+    small:  "(max-width: 736px)",
+    xsmall: "(max-width: 480px)"
+  });
+  skel.layout({
+    containers: 960,
+    breakpoints: {
+      medium: {
+        containers: "90%"
+      },
+      small: {
+        containers: "95%",
+      }
+    }
+  });
 })(jQuery);
