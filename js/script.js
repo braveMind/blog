1633ce932a8ef505bd7b430b84ce3a47a013d447
jun <1042439471@qq.com>
2016-04-09 01:26:59 +0800

    Site updated: 2016-04-09 01:26:59

--- js/script.js
+++ js/script.js
@@ -134,22 +134,7 @@
 
     $container.removeClass('mobile-nav-on');
   });
-  skel.breakpoints({
-    xlarge: "(max-width: 1680px)",
-    large:  "(max-width: 1280px)",
-    medium: "(max-width: 980px)",
-    small:  "(max-width: 736px)",
-    xsmall: "(max-width: 480px)"
-  });
-  skel.layout({
-    containers: 960,
-    breakpoints: {
-      medium: {
-        containers: "90%"
-      },
-      small: {
-        containers: "95%",
-      }
-    }
-  });
+
+  $("#showTag").hover(function(){$("#tags_clod").show()},
+      function(){$("#tags_clod").hide()})
 })(jQuery);
